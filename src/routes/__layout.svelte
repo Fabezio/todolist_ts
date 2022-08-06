5ff47d53cf7ec025b2f0e3787bc5b8287a9e724c
fabezio <fabezio@outlook.fr>
2022-08-06 03:48:45 +0200

    suppression supabase

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,11 +2,11 @@
   import DisplayData from "$lib/components/DisplayData.svelte"
   import "bootstrap/scss/bootstrap.scss";
   import "../app.css";
-  import {supabase} from "../supabase"
+
   // console.log(supabase)
 </script>
 
-<!-- <DisplayData data={supabase.auth} /> -->
+
 <div class="box">
   <slot />
 </div>
